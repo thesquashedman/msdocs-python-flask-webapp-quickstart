fbfbb74be3060988f079ae0f5207c17b0539a053
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2023-05-26 21:07:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
-import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-   app.run()
+def connection():
+    
+    cnxn = mysql.connector.connect(user="thesquashedman", password="Mesoepic2", host="pavelserver.mysql.database.azure.com", port=3306, database="library", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT * FROM test;")
+    
+    row = cursor.fetchone() 
+    while row:
+        print (row) 
+        row = cursor.fetchone()
+    return "Connection to database successful"
