4593ae52cb17a8d0d1a6ade5567b6ba31654bbdd
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2023-05-27 01:10:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def connection():
     
     cnxn = mysql.connector.connect(user="thesquashedman", password="#cooldude2", host="pavelserver.mysql.database.azure.com", port=3306, database="library")
     cursor = cnxn.cursor()
-    cursor.execute("SELECT * FROM test;")
+    cursor.execute("SELECT * FROM book;")
     
 
     html = "<table>"
@@ -16,7 +16,7 @@ def connection():
     while row:
         html += "<tr>"
         for item in row:
-            html += "<td>" + item + "</td>"
+            html += "<td>" + str(item) + "</td>"
         html += "</tr>"
         print (row) 
         row = cursor.fetchone()
